02d0f3bf5e965c19f2ce336a1ba1930f41eefae6
ploysaiplsz <ploypastira@gmail.com>
2022-01-08 02:42:43 +0700

    New version

--- lib/main.dart
+++ lib/main.dart
@@ -55,160 +55,178 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        //TODO: 5.แสดงผลคะแนนที่ได้ กำหนดให้ตอบถูกต้องในแต่ละข้อจะได้ 1 คะแนน (นักเรียนอาจต้องเขียนโปรแกรมในส่วนอื่นด้วย เพื่อสามารถแสดงผลคะแนนที่ถูกต้อง)
-
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                questions[questionNumber],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          //TODO: 5.แสดงผลคะแนนที่ได้ กำหนดให้ตอบถูกต้องในแต่ละข้อจะได้ 1 คะแนน (นักเรียนอาจต้องเขียนโปรแกรมในส่วนอื่นด้วย เพื่อสามารถแสดงผลคะแนนที่ถูกต้อง)
+          if (questionNumber < questions.length) ...[
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    questions[questionNumber],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green
-              ),
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                bool correctAnswer = answers[questionNumber];
-                if (correctAnswer==true) {
-                  setState(() {
-                    score++;
-                    //เมื่อกดปุ่ม True เพิ่มข้อมูลเข้าไปในลิสต์ scoreKeeper โดยใช้ add method
-                    scoreKeeper.add(
-                        Icon(
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.green),
+                  child: Text(
+                    'True',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    bool correctAnswer = answers[questionNumber];
+                    if (correctAnswer == true) {
+                      setState(() {
+                        score++;
+                        //เมื่อกดปุ่ม True เพิ่มข้อมูลเข้าไปในลิสต์ scoreKeeper โดยใช้ add method
+                        scoreKeeper.add(Icon(
                           Icons.check,
                           color: Colors.green,
                         ));
-                    //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
-                    if (questionNumber < questions.length-1) {
-                      questionNumber++;
-                    }
-                    else {
-                      questionNumber = 0;
-                      scoreKeeper.clear();
-                      //TODO: 1.ถ้าคำถามหมดแล้ว ให้เริ่มต้นใหม่ โดยกลับไปที่คำถามเดิมและเคลียร์ scoreKeeper ด้วย
-
-                    }
-
-
-                  });
-                }
-                else{
-                  setState(() {
-                    scoreKeeper.add(
-                        Icon(
+                        //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
+                        //if (questionNumber < questions.length - 1) {
+                        questionNumber++;
+                        //}
+                        // else {
+                        //   questionNumber = 0;
+                        //   scoreKeeper.clear();
+                        //
+                        //
+                        // }
+                      });
+                    } else {
+                      setState(() {
+                        scoreKeeper.add(Icon(
                           Icons.close,
                           color: Colors.red,
                         ));
-                    if (questionNumber < questions.length-1) {
-                      questionNumber++;
+                        //if (questionNumber < questions.length - 1) {
+                        questionNumber++;
+                        //}
+                        // else {
+                        //   questionNumber = 0;
+                        //   scoreKeeper.clear();
+                        // }
+                      });
                     }
-                    else {
-                      questionNumber = 0;
-                      scoreKeeper.clear();
-
-                    }
-                  });
-
-                }
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: ElevatedButton(
-              // color: Colors.red,
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red
-              ),
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+                  },
                 ),
               ),
-              onPressed: () {
-                bool correctAnswer = answers[questionNumber];
-                if (correctAnswer==false) {
-                  setState(() {
-                    score++;
-                    //เมื่อกดปุ่ม True เพิ่มข้อมูลเข้าไปในลิสต์ scoreKeeper โดยใช้ add method
-                    scoreKeeper.add(
-                        Icon(
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ElevatedButton(
+                  // color: Colors.red,
+                  style: ElevatedButton.styleFrom(primary: Colors.red),
+                  child: Text(
+                    'False',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    bool correctAnswer = answers[questionNumber];
+                    if (correctAnswer == false) {
+                      setState(() {
+                        score++;
+                        //เมื่อกดปุ่ม True เพิ่มข้อมูลเข้าไปในลิสต์ scoreKeeper โดยใช้ add method
+                        scoreKeeper.add(Icon(
                           Icons.check,
                           color: Colors.green,
                         ));
-                    //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
-                    if (questionNumber < questions.length-1) {
-                      questionNumber++;
-                    }
-                    else {
-                      questionNumber = 0;
-                      scoreKeeper.clear();
-                      //TODO: 1.ถ้าคำถามหมดแล้ว ให้เริ่มต้นใหม่ โดยกลับไปที่คำถามเดิมและเคลียร์ scoreKeeper ด้วย
-
-                    }
-
-
-                  });
-                }
-                else{
-                  setState(() {
-                    scoreKeeper.add(
-                        Icon(
+                        //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
+                        //if (questionNumber < questions.length - 1) {
+                        questionNumber++;
+                        //}
+                        // else {
+                        //   questionNumber = 0;
+                        //   scoreKeeper.clear();
+                        //
+                        //
+                        // }
+                      });
+                    } else {
+                      setState(() {
+                        scoreKeeper.add(Icon(
                           Icons.close,
                           color: Colors.red,
                         ));
-                    if (questionNumber < questions.length-1) {
-                      questionNumber++;
+                        //if (questionNumber < questions.length - 1) {
+                        questionNumber++;
+                        //}
+                        // else {
+                        //   questionNumber = 0;
+                        //   scoreKeeper.clear();
+                        // }
+                      });
                     }
-                    else {
-                      questionNumber = 0;
-                      scoreKeeper.clear();
-
-                    }
-                  });
-
-                      }
-                //TODO: 3.ปรับปรุงโค้ด เมื่อกดปุ่ม False แล้วให้มีการทำงานในลักษณะเดียวกับปุ่ม True
-
-              },
-
-
+                    //TODO: 3.ปรับปรุงโค้ด เมื่อกดปุ่ม False แล้วให้มีการทำงานในลักษณะเดียวกับปุ่ม True
+                  },
+                ),
+              ),
             ),
-          ),
-        ),
-        //แสดงผล icon สำหรับ scoreKeeper
-        Row(
-          children: scoreKeeper,
-        )
-      ],
-    );
+            //แสดงผล icon สำหรับ scoreKeeper
+            Row(
+              children: scoreKeeper,
+            ),
+            Text('คะแนนที่ได้ : $score')
+          ] else ...[
+            Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Text(
+                      'คะแนนที่ได้ : $score',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.white),
+                  child: Text(
+                    'ลองใหม่อีกครั้ง',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      questionNumber = 0;
+                      score = 0;
+                      scoreKeeper.clear();
+                      //TODO: 1.ถ้าคำถามหมดแล้ว ให้เริ่มต้นใหม่ โดยกลับไปที่คำถามเดิมและเคลียร์ scoreKeeper ด้วย
+                    });
+                  },
+                ),
+              ),
+            )
+          ]
+        ]);
   }
 }
-
